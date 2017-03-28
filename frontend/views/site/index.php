7764166975b05ea94b3abdc0db1892d9d42579ec
Administrator <Administrator@bfyang5130>
2017-03-28 11:59:56 +0800

    增加3D翻转功能

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -5,28 +5,46 @@ $this->title = '寻想网络科技';
 ?>
 <?= $this->render("/layouts/flashPic.php") ?>
 <link href="/css/index.css?v=<?= date("YmdHis") ?>" rel="stylesheet">
-<div class="base-container clear">
+<div class="base-container filp-container clear">
+    <ul>
+        <li>
+            <div class="out-box div3 fl mg-top15">
+                <div class="front-box box index-center-box">
+                    <h1>创新</h1>
+                    <p class="lead pd15">&nbsp;&nbsp;&nbsp;&nbsp;以现有的思维模式提出有别于常规或常人思路的见解为导向,利用现有的知识和物质，在特定的环境中,本着理想化需要或为满足社会需求，而改进或创造原来不存在或不完善的事物、方法、元素、路径、环境，并能获得一定有益效果的行为。</p>
+                </div>
+                <div class="back-box box index-center-box">
+                    <h1>科技</h1>
+                    <p class="lead pd15">&nbsp;&nbsp;&nbsp;&nbsp;解决理论问题，技术解决实际问题。科学要解决的问题，是发现自然界中确凿的事实与现象之间的关系，并建立理论把事实与现象联系起来；技术的任务则是把科学的成果应用到实际问题中去。</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="out-box div3 fl mg-top15">
+                <div class="front-box index-center-box">
+                    <h1>科技</h1>
+                    <p class="lead pd15">&nbsp;&nbsp;&nbsp;&nbsp;解决理论问题，技术解决实际问题。科学要解决的问题，是发现自然界中确凿的事实与现象之间的关系，并建立理论把事实与现象联系起来；技术的任务则是把科学的成果应用到实际问题中去。</p>
+                </div>
+                <div class="back-box box index-center-box">
+                    <h1>创新</h1>
+                    <p class="lead pd15">&nbsp;&nbsp;&nbsp;&nbsp;以现有的思维模式提出有别于常规或常人思路的见解为导向,利用现有的知识和物质，在特定的环境中,本着理想化需要或为满足社会需求，而改进或创造原来不存在或不完善的事物、方法、元素、路径、环境，并能获得一定有益效果的行为。</p>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div class="out-box div3 fl mg-top15">
+                <div class="front-box index-center-box">
+                    <h1>平台</h1>
+                    <p class="lead pd15">&nbsp;&nbsp;&nbsp;&nbsp;教你不仅仅扩大自身影响，同时还要将影响力变为实际效益，并且打造成为终身职业。关键何在？就在于搭建平台。如此简单易行，如此低投入，如此的成功可能，真是前所未有。</p>
 
-    <div class="div3 fl mg-top15">
-        <div class="box index-center-box">
-            <h1>创新</h1>
-            <p class="lead pd15">&nbsp;&nbsp;&nbsp;&nbsp;以现有的思维模式提出有别于常规或常人思路的见解为导向,利用现有的知识和物质，在特定的环境中,本着理想化需要或为满足社会需求，而改进或创造原来不存在或不完善的事物、方法、元素、路径、环境，并能获得一定有益效果的行为。</p>
-        </div>
-    </div>
-    <div class="div3 fl mg-top15">
-        <div class="box index-center-box">
-            <h1>科技</h1>
-            <p class="lead pd15">&nbsp;&nbsp;&nbsp;&nbsp;解决理论问题，技术解决实际问题。科学要解决的问题，是发现自然界中确凿的事实与现象之间的关系，并建立理论把事实与现象联系起来；技术的任务则是把科学的成果应用到实际问题中去。</p>
-        </div>
-    </div>
-    <div class="div3 fl mg-top15">
-        <div class="box index-center-box">
-            <h1>平台</h1>
-            <p class="lead pd15">&nbsp;&nbsp;&nbsp;&nbsp;教你不仅仅扩大自身影响，同时还要将影响力变为实际效益，并且打造成为终身职业。关键何在？就在于搭建平台。如此简单易行，如此低投入，如此的成功可能，真是前所未有。</p>
-
-        </div>
-    </div>
-
+                </div>
+                <div class="back-box box index-center-box">
+                    <h1>创新</h1>
+                    <p class="lead pd15">&nbsp;&nbsp;&nbsp;&nbsp;以现有的思维模式提出有别于常规或常人思路的见解为导向,利用现有的知识和物质，在特定的环境中,本着理想化需要或为满足社会需求，而改进或创造原来不存在或不完善的事物、方法、元素、路径、环境，并能获得一定有益效果的行为。</p>
+                </div>
+            </div>
+        </li>
+    </ul>
 </div>
 <div class="base-container clear">
     <h1>科技案例</h1>
